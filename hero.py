f6188762d51cd7c6ebcd1d429ebb5a8c41df0cf3
itosha <anton.jimson@gmail.com>
2021-12-17 00:09:11 +0300

    Документация

--- hero.py
+++ hero.py
@@ -4,10 +4,34 @@ from music2 import *
 
 
 class Hero(pygame.sprite.Sprite):
+    """
+    Класс Hero используется для создания персонажа.
+    Основное применение - основа для содания идивидуального героя.
+
+    Note:
+        В классах наследующихся от данного для отображения анимации атаки требуется дополнительное описание метода
+        special_attack(), который возврашает две координаты левого верхнего угла картинки.
+
+        Также требуется описание метода обродатывающего события клавиатуры и мыши (wasd) тем или иным способом.
+        Ключевым является преобразование этих данных в соответствующие атрибуты класса.
+
+    Attributes
+        image - переменная типа pygame.Surface содержающая изображение персонажа на данный момент.
+        rect - хитбокс персонажа.
+        attack_rect - зона поражения мили атаки.
+        Далее ислользуется ряд объектов класа get_sprites обслуживающих анимацию соответствующих действий.
+        Также используется переменные состояния и фаз персонажа.
+    """
     def __init__(self, start_x, start_y, hero_size=(HERO_SiZE_X, HERO_SIZE_Y)):
+        """
+        Конструкор класса
+        :param start_x: начальное положение
+        :param start_y: начальное положение
+        :param hero_size: характерные размеры персонажа которые определяют хитбокс персонажа
+        """
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface(hero_size)
-        self.hero_size = hero_size
+        # self.hero_size = hero_size
         self.run_sprite = Sprites(run_sprite1)
         self.stay_sprite = Sprites(stay_sprite1)
         self.milli_attack_sprite = Sprites(milli_attack_sprite1, 40, 2)
@@ -22,10 +46,10 @@ class Hero(pygame.sprite.Sprite):
         self.Vx = 0
         self.Vy = 0
         self.kick_speed = 0
-        self.running = False
+        # self.running = False
         self.attack = False
         self.stun = False
-        self.shotting = False
+        self.shooting = False
         self.power_bar = Sprites('s_pow_bar_strip10.png', 2000, 1, True)
         self.power_bar_rect = self.power_bar.sprite.get_rect()
         self.power_bar.currentFrame = 4
@@ -38,6 +62,11 @@ class Hero(pygame.sprite.Sprite):
         self.death = False
 
     def update(self, platform, screen):
+        """
+        Главный метод класса выполняющийся циклически.
+        :param platform: список объектов, с которыми проверяются столкновения героя
+        :param screen: поверхность где рисуем персонажа
+        """
         if not self.death:
             self.event_handling()
             self.power = self.power_bar.currentFrame
@@ -57,12 +86,15 @@ class Hero(pygame.sprite.Sprite):
         self.reset()
 
     def faze_checking(self):
+        """
+        Проверяет фазы персонажа и отвечает за расход энергии по совместительству (выстрелы и вход в атаку)
+        """
         if self.lives > 0:
             if not self.attack and self.milli_attack and self.power > 3:
                 self.attack = True
                 punch_music()
                 self.power_bar.currentFrame -= 4
-            elif self.shotting and self.power > 1:
+            elif self.shooting and self.power > 1:
                 self.shot()
                 self.power_bar.currentFrame -= 2
         else:
@@ -71,6 +103,10 @@ class Hero(pygame.sprite.Sprite):
             characters.remove(self)
 
     def hitcheck(self):
+        """
+        Проверка попадания в героя чужого 'кулака' (пересечение с чужой зоной поражения)
+        Также описывает последствия попадания (стан и отскок) (снимает хп)
+        """
         for obj in characters:
             if obj.attack and not (self is obj):
                 if self.rect.colliderect(obj.attack_rect):
@@ -86,11 +122,20 @@ class Hero(pygame.sprite.Sprite):
                         self.stun = True
 
     def shot(self):
+        """
+        Создает новый объект класса bullet в результате выстрела
+        """
         all_sprites.add(Bullet(self, self.rect.centerx, self.rect.centery))
         fireball_music()
-        self.shotting = False
+        self.shooting = False
 
     def animate(self):
+        """
+        Основной метод отвечающий за анимацию всего связанного с героем
+        и проверки какую имено анимацию следует использовать(полоска здоровья и энергии, и сам персонаж)
+        Также выводит из состаяния атаки (в методе special_attack это необходимо прописать)
+        :return: Положение верхнего левого угла картинки
+        """
         if self.power_bar.currentFrame < self.power_bar.numbers_image - 1:
             self.power_bar.get_sprite()
         else:
@@ -117,6 +162,10 @@ class Hero(pygame.sprite.Sprite):
             return self.rect.y, self.rect.x
 
     def death_animate(self):
+        """
+        Специальная анимация при смерти. Доп метод служит для уменьшения проверок
+        :return: Положение верхнего левого угла картинки
+        """
         self.health_bar_rect.midbottom = self.rect.midtop
         # self.power_bar_rect.bottomleft = self.health_bar_rect.topleft
         self.image = self.death_sprite.get_sprite()
@@ -126,6 +175,10 @@ class Hero(pygame.sprite.Sprite):
         ...
 
     def event_handling(self):
+        """
+        обработка событий уже считанным с клавиатуры и после этого присвоение пременной
+        onGround значения False для коректной проверки столкновений
+        """
         if not self.stun:
             if self.right:
                 self.Vx += MOVE_SPEED
@@ -137,6 +190,10 @@ class Hero(pygame.sprite.Sprite):
         self.onGround = False
 
     def collision_x(self, platform):
+        """
+        проверка столнконовения героя с препятствиями(или другими героями) по горизонтали
+        :param platform: объекты, с которыми проверяются столкновения
+        """
         for p in platform:
             if pygame.sprite.collide_rect(self, p):
 
@@ -157,6 +214,10 @@ class Hero(pygame.sprite.Sprite):
                         self.rect.left = p.rect.right
 
     def collision_y(self, platform):
+        """
+        проверка столнконовения героя с препятствиями(или другими героями) по вертикали
+        :param platform: объекты, с которыми проверяются столкновения
+        """
         for p in platform:
             if pygame.sprite.collide_rect(self, p):
                 if self.Vy > 0:
@@ -181,6 +242,10 @@ class Hero(pygame.sprite.Sprite):
                         self.Vy = 0
 
     def movement(self, platform):
+        """
+        Перемешения персонажа
+        :param platform: транзит параметра для collision
+        """
         self.Vx += self.kick_speed
         self.rect.x += self.Vx
         self.collision_x(platform)
@@ -190,8 +255,11 @@ class Hero(pygame.sprite.Sprite):
             self.collision_y(platform)
 
     def reset(self):
+        """
+        Сброс значение переменных или их откат
+        """
         self.Vx = 0
-        self.milli_attack = self.shotting = False
+        self.milli_attack = self.shooting = False
         if self.kick_speed > 0:
             self.kick_speed -= 1
         elif self.kick_speed < 0:
@@ -199,6 +267,9 @@ class Hero(pygame.sprite.Sprite):
 
 
 class Hero1(Hero):
+    """
+    Первый герой(типо монстрик)
+    """
     def __init__(self, start_x, start_y):
         super(Hero1, self).__init__(start_x, start_y)
         self.jump_sprite = Sprites(jump_sprite1)
@@ -209,9 +280,13 @@ class Hero1(Hero):
         self.death_sprite = Sprites(death_sprite1)
 
     def event_checking_hero(self, event):
+        """
+        обработка событий с манипуляторов
+        :param event: объект события pygame
+        """
         if event.type == MOUSEBUTTONDOWN:
             if event.button == 1:
-                self.shotting = True
+                self.shooting = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a or event.key == a_key_rus:
                 self.left, self.FACING = True, True
@@ -220,7 +295,7 @@ class Hero1(Hero):
             elif event.key == pygame.K_w or event.key == w_key_rus:
                 self.jump = True
             elif event.key == pygame.K_e or event.key == e_key_rus:
-                self.shotting = True
+                self.shooting = True
             elif event.key == pygame.K_q or event.key == q_key_rus or event.key == K_SPACE:
                 self.milli_attack = True
         if event.type == pygame.KEYUP:
@@ -232,6 +307,10 @@ class Hero1(Hero):
                 self.jump = False
 
     def special_attack(self):
+        """
+        атака анимация
+        :return: верхний левый угол
+        """
         self.image = self.milli_attack_sprite.get_sprite(self.FACING)
         if self.milli_attack_sprite.currentFrame == self.milli_attack_sprite.numbers_image - 1:
             self.attack = False
@@ -244,6 +323,9 @@ class Hero1(Hero):
 
 
 class Hero2(Hero):
+    """
+    Второй герой (сейчас используется нарисованная Сардором версия спрайтов)
+    """
     def __init__(self, start_x, start_y):
         super(Hero2, self).__init__(start_x, start_y)
         self.jump_sprite = Sprites(jump_sprite3)
@@ -254,9 +336,13 @@ class Hero2(Hero):
         self.death_sprite = Sprites(death_sprite3, 50)
 
     def event_checking_hero(self, event):
+        """
+        обработка событий с манипуляторов
+        :param event: объект события pygame
+        """
         if event.type == MOUSEBUTTONDOWN:
             if event.button == 3:
-                self.shotting = True
+                self.shooting = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 self.left, self.FACING = True, True
@@ -267,7 +353,7 @@ class Hero2(Hero):
             elif event.key == pygame.K_SLASH:
                 self.milli_attack = True
             elif event.key == pygame.K_COMMA or event.key == comma_key_rus:
-                self.shotting = True
+                self.shooting = True
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 self.left = False
@@ -277,6 +363,10 @@ class Hero2(Hero):
                 self.jump = False
 
     def special_attack(self):
+        """
+        атака анимация
+        :return: верхний левый угол
+        """
         self.image = self.milli_attack_sprite.get_sprite(self.FACING)
         if self.milli_attack_sprite.currentFrame == self.milli_attack_sprite.numbers_image - 1:
             self.attack = False
